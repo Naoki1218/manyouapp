369838ce70b9799be1aeea797188e748aa57b26d
nakajima naoki <naoki9890@yahoo.co.jp>
2020-10-14 21:03:12 +0900

    step1 test finished

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
-  validates :title,  presence: true
+  validates :title, presence: true
   validates :task_limit_on, presence: true
   validates :task_status, presence: true
+
+  enum task_status: { 未: 0, 済: 1 }
 end
