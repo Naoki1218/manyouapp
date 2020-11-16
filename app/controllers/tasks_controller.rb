cecd39d8b573796e5d2aab560a6119e8ca6cb524
nakajima naoki <naoki9890@yahoo.co.jp>
2020-11-16 21:11:55 +0900

    step4 test done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,72 +3,70 @@ class TasksController < ApplicationController
 
   def index
     if params[:title].present? && params[:task_status].present?
-      @tasks = Task.get_by_title(params[:title]).get_by_task_status(params[:task_status]).page(params[:page]).per(5)
+      @tasks = current_user.tasks.get_by_title(params[:title]).get_by_task_status(params[:task_status]).page(params[:page]).per(5)
     elsif params[:title].present?
-      @tasks = Task.get_by_title(params[:title]).page(params[:page]).per(5)
+      @tasks = current_user.tasks.get_by_title(params[:title]).page(params[:page]).per(5)
     elsif params[:task_status].present?
-      @tasks = Task.get_by_task_status(params[:task_status]).page(params[:page]).per(5)
+      @tasks = current_user.tasks.get_by_task_status(params[:task_status]).page(params[:page]).per(5)
     elsif params[:sort_expired]
-      @tasks = Task.order(task_limit_on: "DESC").page(params[:page]).per(5)
+      @tasks = current_user.tasks.order(task_limit_on: "DESC").page(params[:page]).per(5)
     elsif params[:sort_priority]
-      @tasks = Task.order(priority:"DESC").page(params[:page]).per(5)
+      @tasks = current_user.tasks.order(priority:"DESC").page(params[:page]).per(5)
     else
-      @tasks = Task.order(id: "DESC").page(params[:page]).per(5)
+      @tasks = current_user.tasks.order(id: "DESC").page(params[:page]).per(5)
     end
   end
 
-def new
-  @task = Task.new
-end
-
-def create
-  @task = Task.new(task_params)
-  # if params[:back]
-  #   render :new
-  # else
-  if @task.save
-    # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
-    redirect_to tasks_path, notice: "タスクを作成しました！"
-  else
-    # 入力フォームを再描画します。
-    render :new
-    # end
+  def new
+    @task = Task.new
   end
-end
 
-def show
-  @task = Task.find(params[:id])
-end
+  def create
+    @task = current_user.tasks.build(task_params)
 
-def edit
-  @task = Task.find(params[:id])
-end
-
-def update
-  @task = Task.find(params[:id])
-  if @task.update(task_params)
-    redirect_to tasks_path, notice: "タスクを編集しました！"
-  else
-    render :edit
+    if @task.save
+      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+      redirect_to tasks_path, notice: "タスクを作成しました！"
+    else
+      # 入力フォームを再描画します。
+      render :new
+      # end
+    end
   end
-end
 
-def destroy
-  @task.destroy
-  redirect_to tasks_path, notice:"タスクを削除しました！"
-end
+  def show
+    @task = current_user.tasks.find(params[:id])
+  end
 
-# def confirm
-#   @task = Task.new(task_params)
-#   render :new if @task.invalid?
-# end
+  def edit
+    @task = current_user.tasks.find(params[:id])
+  end
 
-private
-def task_params
-  params.require(:task).permit(:title, :task_limit_on, :task_status, :priority)
-end
+  def update
+    @task = current_user.tasks.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました！"
+    else
+      render :edit
+    end
+  end
 
-def set_task
-  @task = Task.find(params[:id])
-end
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"タスクを削除しました！"
+  end
+
+  # def confirm
+  #   @task = Task.new(task_params)
+  #   render :new if @task.invalid?
+  # end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :task_limit_on, :task_status, :priority)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,8 @@ class Task < ApplicationRecord
   validates :task_status, presence: true
   validates :priority, presence: true
 
+  belongs_to :user, optional: true
+
   enum task_status: { 未着手: 0, 着手中: 1, 完了: 2 }
   enum priority: { 選択して下さい: 0, 低: 1, 中: 2, 高: 3 }
 
